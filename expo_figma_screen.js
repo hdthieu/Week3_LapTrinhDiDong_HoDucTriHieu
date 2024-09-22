d0e94840a1e0dfc76e20e46d4e961a2d8a677505
HoDucTriHieu <hoductrihieu@gmail.com>
2024-09-21 17:50:27 -0700

    week3 reactnavtive expo ex1 screen2 update

--- expo_figma_screen.js
+++ expo_figma_screen.js
@@ -1,44 +1,73 @@
 import React from 'react';
 import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
-const LoginScreen = () => {
+const LoginScreen2 = () => {
   return (
-    <View style={{ flex: 1, backgroundColor: '#00CCF9' }}>
-      <View style={{flex: 1, justifyContent: 'center', alignItems:'center', top: 67}}>
-        <View style={styles.outerCircle}>
-          <View style={styles.innerCircle}></View>
+    <View style={{ flex: 1 }}>
+      <LinearGradient
+        style={{ flex: 1 }}
+        colors={['#C7F4F7', '#D1F4F6', '#E5F4F5', '#37D6F8', '#00CCF9']}>
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            top: 67,
+          }}>
+          <View style={styles.outerCircle}>
+            <View style={styles.innerCircle}></View>
+          </View>
         </View>
-      </View>
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', top: 70 }}>
-        <Text style={[styles.baseText, styles.titleText]}>GROW </Text>
-        <Text style={[styles.baseText, styles.titleText]}>YOUR BUSINESS</Text>
-      </View>
-      <View
-        style={{
-          flex: 1,
-          alignItems: 'center',
-          justifyContent: 'center',
-          margin: '5%',
-        }}>
-        <Text style={[styles.baseText, styles.textDescription]}>
-          We will help you to grow your business using online server
-        </Text>
-      </View>
-      <View
-        style={{
-          flex: 1,
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'space-evenly',
-        }}>
-        <TouchableOpacity style={[styles.button, { backgroundColor: '#E3C000' }]}>
-          <Text style={styles.buttonText}>LOGIN</Text>
-        </TouchableOpacity>
-     
-        <TouchableOpacity style={[styles.button, { backgroundColor: '#E3C000' }]}>
-          <Text style={styles.buttonText}>SIGN UP</Text>
-        </TouchableOpacity>
-      </View>
+        <View
+          style={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+            top: 70,
+          }}>
+          <Text style={[styles.baseText, styles.titleText]}>GROW </Text>
+          <Text style={[styles.baseText, styles.titleText]}>YOUR BUSINESS</Text>
+        </View>
+        <View
+          style={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+            margin: '5%',
+          }}>
+          <Text style={[styles.baseText, styles.textDescription]}>
+            We will help you to grow your business using online server
+          </Text>
+        </View>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-evenly',
+          }}>
+          <TouchableOpacity
+            style={[styles.button, { backgroundColor: '#E3C000' }]}>
+            <Text style={styles.buttonText}>LOGIN</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.button, { backgroundColor: '#E3C000' }]}>
+            <Text style={styles.buttonText}>SIGN UP</Text>
+          </TouchableOpacity>
+        </View>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'row',
+            justifyContent: 'center',
+            
+          }}>
+          <Text style={{fontSize: 18,
+            fontWeight: '700',
+            fontFamily: 'Roboto',}}>HOW WE WORK</Text>
+        </View>
+      </LinearGradient>
     </View>
   );
 };
@@ -52,7 +81,7 @@ const styles = StyleSheet.create({
     borderRadius: 5,
   },
   buttonText: {
-    color: 'dark',
+    color: 'dark', 
     fontSize: 18,
     fontWeight: '700',
   },
@@ -71,7 +100,7 @@ const styles = StyleSheet.create({
   outerCircle: {
     width: 140,
     height: 140,
-    backgroundColor: '#00c8ff',
+    // backgroundColor: '#00c8ff',
     borderRadius: 70,
     justifyContent: 'center',
     alignItems: 'center',
@@ -79,11 +108,11 @@ const styles = StyleSheet.create({
   innerCircle: {
     width: 120,
     height: 120,
-    backgroundColor: '#00c8ff',
+    // backgroundColor: '#00c8ff',
     borderRadius: 60,
     borderWidth: 10,
     borderColor: 'black',
   },
 });
 
-export default LoginScreen;
+export default LoginScreen2;
